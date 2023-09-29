c322779e3a6a1bdf57b2ee99d4be4886e077acf1
Usman <buttu7666@gmail.com>
2023-09-30 00:23:10 +0500

    completed (unpolished)

--- script.js
+++ script.js
@@ -6,35 +6,58 @@ tick.src = "./resources/tick.png";
 cross.src = "./resources/cross.png";
 
 emptyBoxes.forEach(box => {
-    box.addEventListener("click", ()=> {
-            box.style.backgroundImage = "url(" + tick.src + ")";
-            box.classList.remove("empty");
-            box.classList.add("tick");
-            game();
-    })
+    box.addEventListener("click", clickHandler, {once: true})
 });
 
-function game() {
-    let randomSelection = Math.floor(Math.random() *emptyBoxes.length);
-    setTimeout(() => {
-        emptyBoxes[randomSelection].style.backgroundImage = "url(" + cross.src + ")";;
-    }, 200);
+function clickHandler() {
+    this.style.backgroundImage = "url(" + tick.src + ")";
+    this.classList.remove("empty");
+    this.classList.add("tick");
+    game();
     win();
 }
 
+
+function game() {
+    emptyBoxes = document.querySelectorAll(".empty");
+    let randomSelection = Math.floor(Math.random() *emptyBoxes.length);
+    let box = emptyBoxes[randomSelection];
+    box.style.backgroundImage = "url(" + cross.src + ")";
+    box.classList.remove("empty");
+    box.classList.add("cross");
+    box.removeEventListener("click", clickHandler)
+}
+
 function win() {
-    if(horizontalCheck() ||  verticalCheck()) {
+    if(horizontalCheck() == "player" ||  verticalCheck() =="player" || diagonalCheck() == "player") {
+        emptyBoxes.forEach(box => {
+            box.removeEventListener("click", clickHandler, {once: true})
+        });
         setTimeout(() => {
             alert("You win");
         }, 0);
     }
+    else if(horizontalCheck() == "enemy" ||  verticalCheck() == "enemy" || diagonalCheck() == "enemy"){
+        emptyBoxes.forEach(box => {
+            box.removeEventListener("click", clickHandler, {once: true})
+        });
+        setTimeout(() => {
+            alert("You Lost");
+        }, 0);
+    }
+
 }
 
 
 function horizontalCheck(){
     for(let i = 0; i < 9; i+=3) {
         if(gridBoxes[i].classList.contains("tick") && gridBoxes[i+1].classList.contains("tick") && gridBoxes[i+2].classList.contains("tick")){
-            return true;
+            console.log("one");
+            return "player";
+        }
+        else if(gridBoxes[i].classList.contains("cross") && gridBoxes[i+1].classList.contains("cross") && gridBoxes[i+2].classList.contains("cross")){
+            console.log("one");
+            return "enemy";
         }
     }
 }
@@ -42,14 +65,48 @@ function horizontalCheck(){
 function verticalCheck(){
     for(let i = 0; i < 3; i++) {
         if(gridBoxes[i].classList.contains("tick") && gridBoxes[i+3].classList.contains("tick") && gridBoxes[i+6].classList.contains("tick")){
-            return true;
+            gridBoxes[i].style.backgroundColor = "#3fc03f";
+            gridBoxes[i+3].style.backgroundColor = "#3fc03f";
+            gridBoxes[i+6].style.backgroundColor = "#3fc03f";
+            return "player";
+        }
+        else if(gridBoxes[i].classList.contains("cross") && gridBoxes[i+3].classList.contains("cross") && gridBoxes[i+6].classList.contains("cross")){
+            gridBoxes[i].style.backgroundColor = "#d34c4c";
+            gridBoxes[i+3].style.backgroundColor = "#d34c4c";
+            gridBoxes[i+6].style.backgroundColor = "#d34c4c";
+            return "enemy";
         }
     }
 }
 
+function diagonalCheck() {
+    if(gridBoxes[4].classList.contains("tick")) {
+        if(gridBoxes[0].classList.contains("tick") && gridBoxes[8].classList.contains("tick") ) {
+            gridBoxes[4].style.backgroundColor = "#3fc03f";
+            gridBoxes[0].style.backgroundColor = "#3fc03f";
+            gridBoxes[8].style.backgroundColor = "#3fc03f";
+            return "player";
+        }
+        else if (gridBoxes[6].classList.contains("tick") && gridBoxes[2].classList.contains("tick")) {
+            gridBoxes[4].style.backgroundColor = "#3fc03f";
+            gridBoxes[6].style.backgroundColor = "#3fc03f";
+            gridBoxes[2].style.backgroundColor = "#3fc03f";
+            return "player";
+        }
+    }
 
-function boxCheck(box) {
-    if(box.style.backgroundImage != "url(" + cross.src + ")"){ 
-        return true;
+    else if(gridBoxes[4].classList.contains("cross")) {
+        if(gridBoxes[0].classList.contains("cross") && gridBoxes[8].classList.contains("cross")) {
+            gridBoxes[4].style.backgroundColor = "#d34c4c";
+            gridBoxes[0].style.backgroundColor = "#d34c4c";
+            gridBoxes[8].style.backgroundColor = "#d34c4c";
+            return "enemy";
+        }
+        else if(gridBoxes[6].classList.contains("cross") && gridBoxes[2].classList.contains("cross")) {
+            gridBoxes[4].style.backgroundColor = "#d34c4c";
+            gridBoxes[6].style.backgroundColor = "#d34c4c";
+            gridBoxes[2].style.backgroundColor = "#d34c4c";
+            return "enemy";
+        }
     }
 }
